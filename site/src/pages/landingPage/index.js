9e6a4e490201d53dd25cca51ba6ecca9781b3c4a
Giuseppe Pallazini <Pepeteste1883@gmail.com>
2023-02-28 16:17:42 -0300

    Principalmente Icons e criação da common

--- site/src/pages/landingPage/index.js
+++ site/src/pages/landingPage/index.js
@@ -1,14 +1,31 @@
 import './index.scss'
-import LogoBranca from '../../assets/image/logo-branco.png'
+import '../../assets/common/common.scss'
+import LogoBranca from '../../assets/image/logo-branco.png';
+import IconBusca from '../../assets/image/busca.svg';
+import IconUser from '../../assets/image/iconUser.svg';
+import IconMoon from '../../assets/image/iconMoon.svg'
 
 export default function Index() {
-    return(
+    return (
         <main className='main-LandingPage' >
 
             <header className='cabecalho'>
-                <img src={LogoBranca} alt='' />
 
-            
+                <label>
+                    <img src={LogoBranca} alt='' />
+                    <img src={IconBusca} alt='' />
+                </label>
+
+                <div className='cabecalho-div2'>
+                    <div className='cabecalho-div-darkMode'> 
+                        <img src={IconMoon} alt='' />
+                        
+
+                    </div>
+
+                    <img src={IconUser} alt='' />
+                </div>
+
             </header>
 
         </main>
